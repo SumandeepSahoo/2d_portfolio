25b83a27d1c5be386686236498146666f78326b8
SumandeepSahoo <sumandeepsahoo833@gmail.com>
2025-08-20 23:02:25 +0530

    Initial commit - 2D portfolio

---
+++ src/constants.js
@@ -0,0 +1,32 @@
+export const scaleFactor = 4;
+
+export const dialogueData = {
+  pc: `This is my PC. Most of my time goes into learning and building projects in web development.  
+  I mainly use JavaScript and React, and I’m also exploring backend tech like Express and MongoDB.  
+  You can check out my work here on my <a href="https://github.com/SumandeepSahoo" target="_blank">GitHub</a>!`,
+
+  "cs-degree": `I'm currently pursuing my B.Tech in Information Technology at KIIT University.  
+  It's been an exciting journey so far, full of learning and hands-on projects that push me to grow as a developer.`,
+
+  "sofa-table": `That’s my sofa. I usually relax here by watching coding tutorials, tech talks, or sometimes just chilling.  
+  A lot of my ideas for projects actually come while I’m casually browsing tech content online.`,
+
+  tv: `This is my TV. I often follow programming YouTubers and tech creators for inspiration.  
+  They motivate me to learn new technologies and improve my coding skills every day.`,
+
+  bed: `This is where I rest, but honestly I often end up brainstorming project ideas here.  
+  If something exciting strikes, I make sure to note it down so I can work on it later.`,
+
+  resume: `This is my desk and on it is my resume.  
+  <a href="https://drive.google.com/file/d/10BP43u0NLR-j4fVfOFzWai1ZRPaYKfDR/view?usp=sharing" target="_blank">Check it out?</a>  
+  You can also connect with me on <a href="https://www.linkedin.com/in/sumandeep-sahoo-96876b242/" target="_blank">LinkedIn</a> if you’d like to reach out!`,
+
+  projects: `Info about this portfolio: It’s built with Kaboom.js, a JavaScript game library that lets you make fun 2D games.  
+  Instead of a traditional portfolio, I wanted something creative and interactive.  
+  It’s still a work in progress, but I’m learning and improving as I go!`,
+
+  library: `These are my books and resources. As a student, I’m constantly learning from both textbooks and online resources.  
+  I may not have read them all fully, but each one adds something new to my understanding of tech.`,
+
+  exit: `If you’d like to exit my portfolio, just close the tab. Thanks for visiting my little 2D world 🙂`,
+};
